8eef498063fe128bf7cb5b78811d016de5c4e1a0
reddirthorse <operstu1@gmail.com>
2021-11-27 15:43:35 +0900

    first Time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import GetTraffic from './traffic';
+import Input from './input';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [start,setStart] = useState('');
+  const [end,setEnd] = useState('')
+
+
+  const handleChange = (e) =>{
+    const {name, value} = e.target
+
+    name ==='start'? setStart(value) : setEnd(value)
+    console.log(name,value)
+  }
+
+
+  return(
+    <div>
+      <Input name = 'start' type = 'text' placeholder ='' onChange = {handleChange}></Input>
+      <GetTraffic></GetTraffic>
     </div>
-  );
+  )
+
+  
 }
 
 export default App;
