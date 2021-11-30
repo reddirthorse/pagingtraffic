b992823cc941d554423226b2bf4c8a258a2352c0
reddirthorse <operstu1@gmail.com>
2021-11-30 16:23:48 +0900

    11-29

--- src/App.js
+++ src/App.js
@@ -1,44 +1,99 @@
 import './App.css';
 import { useState,useEffect } from 'react';
 import GetTraffic from './traffic';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Input from './input';
 import GetMultiTraffic from './multiTraffic';
 
 function App() {
-  const [start,setStart] = useState('101');
-  const [end,setEnd] = useState('115')
+  const [inputs,setInputs] = useState({
+    startCity:'',
+    endCity:''
+  })
+  const [start,setStart] = useState([]);
+  const [end,setEnd] = useState([])
 
+
+  const {startCity, endCity} = inputs;
 
   const handleChange = (e) =>{
-    const {name, value} = e.target
-    if (name ==='start'){
-      if(value==='서울'){
-        setStart(['101'])
-      }
-      else if(value ==='대구'){
-        setStart(['131','133'])
-      }
-    }else{
-      if(value==='대전'){
-        setEnd(['115'])
-      } 
-      if(value === '부산'){
-        setEnd(['133','140'])
-      }
-    }
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+   
   }
 
   useEffect(() =>{
+    //경부 고속도로 하행선 시작
+    if (startCity ==='서울' && endCity ==='대전'){
+      setStart(['101'])
+      setEnd(['115'])
+    }
+    else if(startCity ==='서울' && endCity ==='대구'){
+      setStart(['101'])
+      setEnd(['129'])
+    }
+    else if(startCity ==='서울' && endCity ==='부산'){
+      setStart(['101','129','131','133'])
+      setEnd(['129','131','133','140'])
+    }else if(startCity ==='대전' && endCity ==='대구'){
+      setStart(['115'])
+      setEnd(['129'])
+    }
+    else if(startCity ==='대전' && endCity ==='부산'){
+      setStart(['115','129','131','133'])
+      setEnd(['129','131','133','140'])
+    }
+    else if(startCity ==='대구' && endCity ==='부산'){
+      setStart(['131','133'])
+      setEnd(['133','140'])
+    }
+    //경부 고속 도로 하행선 종료
+    //경부 고속도로 상행선 시작
+    else if (startCity ==='부산' && endCity ==='대구'){
+      setStart(['133','140'])
+      setEnd(['131','133'])
+    }
+    else if (startCity ==='부산' && endCity ==='대전'){
+      setStart(['123','131','133','140'])
+      setEnd(['115','123','131','133'])
+    }
+    else if (startCity ==='부산' && endCity ==='서울'){
+      setStart(['123','131','133','140'])
+      setEnd(['101','123','131','133'])
+    }
+    else if (startCity ==='대구' && endCity ==='대전'){
+      setStart(['123'])
+      setEnd(['115'])
+    }
+    else if (startCity ==='대구' && endCity ==='서울'){
+      setStart(['123'])
+      setEnd(['101'])
+    }
+    else if (startCity ==='대전' && endCity ==='서울'){
+      setStart(['115'])
+      setEnd(['101'])
+    }
+    //경부 고속도로 상행선 종료
+    //광주 관련 도로 시작
+
+    //광주 관련 도로 종료
     console.log(start)
     console.log(end)
-  },[start])
-
+    
+  },[inputs])
+  console.log(inputs)
+  console.log(startCity)
+  console.log(endCity)
+  console.log(start)
+  console.log(end)
   return(
     <div>
-      <Input name = 'start' type = 'text' placeholder ='' onChange = {handleChange}></Input>
-      <Input name = 'end' type = 'text' placeholder ='' onChange = {handleChange}></Input>
-      <GetTraffic start = {start} end = {end}></GetTraffic>
-      <GetMultiTraffic start = {start} end = {end}></GetMultiTraffic>
+      <Input name = 'startCity' type = 'text' placeholder ='' onChange = {handleChange}></Input>
+      <Input name = 'endCity' type = 'text' placeholder ='' onChange = {handleChange}></Input>
+      <GetMultiTraffic startCity = {startCity} endCity = {endCity} start = {start} end = {end}></GetMultiTraffic>
     </div>
   )
 
--- src/multiTraffic.js
+++ src/multiTraffic.js
@@ -2,17 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
  function GetMultiTraffic(props){
-    let dataList = []
-    let dataSumList = []
+    const [dataSumList,setDataSumList] = useState([])
+    // let dataList = []
+    // let dataSumList = []
     let i=0;
     console.log(props)
+
+    function timeCal(t){
+        let minutes = 0;
+        let hour = 0;
+        if (3600 > t ){
+            minutes = parseInt(t/60)
+            return `${minutes}분` 
+        }
+        else if (t>=3600){
+            hour = parseInt(t/3600)
+            minutes = parseInt((t - hour*3600)/60)
+            return `${hour}시간 ${minutes}분`
+        }
+    }
 
 
     useEffect( async()=>{
         console.log(props)
         const { start, end } = props
-        dataList = []
-        dataSumList = []
+        let tempDataList = []
         let sumTimeAvg = 0
         let sumTimeMax = 0
         let sumTimeMin = 0
@@ -26,21 +40,20 @@ import axios from "axios";
                 
                 for (let j = 0; j<list.length; j++) {
                     if(list[j].startUnitCode ===`${start[i]} `&&list[j].endUnitCode ===`${end[i]} `&&list[j].carType ==='1'){
-                        dataList.push({'carType':list[j].carType,'startUnitCode':list[j].startUnitCode,'endUnitCode':list[j].endUnitCode,'timeAvg':list[j].timeAvg,'timeMax':list[j].timeMax,'timeMin':list[j].timeMin,'sumTm':list[j].sumTm});
+                        tempDataList.push({'carType':list[j].carType,'startUnitCode':list[j].startUnitCode,'endUnitCode':list[j].endUnitCode,'timeAvg':list[j].timeAvg,'timeMax':list[j].timeMax,'timeMin':list[j].timeMin,'sumTm':list[j].sumTm});
                     }
                 }
              }
-             for (let j = 0; j < dataList.length;j++){
-                sumTimeAvg = sumTimeAvg + parseInt(dataList[j].timeAvg)
-                sumTimeMax = sumTimeMax + parseInt(dataList[j].timeMax)
-                sumTimeMin = sumTimeMin + parseInt(dataList[j].timeMin)
+             for (let j = 0; j < tempDataList.length;j++){
+                sumTimeAvg = sumTimeAvg + parseInt(tempDataList[j].timeAvg)
+                sumTimeMax = sumTimeMax + parseInt(tempDataList[j].timeMax)
+                sumTimeMin = sumTimeMin + parseInt(tempDataList[j].timeMin)
             }
 
             // async function getTraffics(){
             // const res = await axios.all(url);
             // console.log(res);
-            dataSumList.push(sumTimeAvg,sumTimeMax,sumTimeMin)
-            console.log(dataList)
+            setDataSumList([sumTimeAvg,sumTimeMax,sumTimeMin])
             console.log(dataSumList)
             console.log(sumTimeMin)
         }
@@ -49,19 +62,17 @@ import axios from "axios";
     catch(e){
             console.log('err')
           }
-        })
+        },[props])
 
+        console.log(dataSumList)
  return(
 
-     <div>
-          {dataList
-         .map((d,cnt)=>{
-             return(
-                 <div>
-                     
-                 </div>
-             )
-         })} 
+     <div className = "col-md-12">
+        <div className = 'startCity'>출발 : {props.startCity}</div>
+        <div className = 'endCity'>도착 : {props.endCity}</div>
+        <div className = 'sumTimeAvg'>평균예상시간 : {timeCal(dataSumList[0])}</div>
+        <div className = 'sumTimeMax'>최대예상시간 : {timeCal(dataSumList[1])}</div>
+        <div className = 'sumTimeMin'>최소예상시간 : {timeCal(dataSumList[2])}</div>
      </div>
  )
 }
